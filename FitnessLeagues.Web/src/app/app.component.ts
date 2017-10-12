ea24c262117e32966ce0940126e24ad6b3a33b5d
Rahul Kulkarni <kulkarnirahul.17@hotmail.com>
2017-10-12 08:07:29 -0400

    Changed the style associated

--- FitnessLeagues.Web/src/app/app.component.ts
+++ FitnessLeagues.Web/src/app/app.component.ts
@@ -3,10 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>
-      Welcome to {{title}}!!
+    <div class="bg-primary text-white" style="border-left: 3em">
+      <h1>
+        Welcome to {{title}}!!      
+      </h1>
       <dashboard-home></dashboard-home>
-    </h1>
+    </div>
   `,
   styles: []
 })
