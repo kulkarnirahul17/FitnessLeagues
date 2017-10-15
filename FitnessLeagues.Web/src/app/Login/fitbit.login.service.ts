0a6a8ee13f1831876f92b9b604d5d33d18ee769e
Rahul Kulkarni <kulkarnirahul.17@hotmail.com>
2017-10-15 19:21:23 -0400

    Added the dummy http call to api

--- FitnessLeagues.Web/src/app/Login/fitbit.login.service.ts
+++ FitnessLeagues.Web/src/app/Login/fitbit.login.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from "@angular/core";
+import { Injectable, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 
-export class FitbitLoginService{
+export class FitbitLoginService {
+    _httpClient: HttpClient;
+
+    constructor(public httpClient: HttpClient)
+    {
+        this._httpClient = httpClient;
+    }
+
     login(): string {
+        var data = this._httpClient.get("https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=228PXF&redirect_uri=http%3A%2F%2Flocalhost%3A59695&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800")
+        .subscribe(data => {
+            alert(JSON.stringify(data));
+        });
         return "Logged in through service";
     }
 }
--- FitnessLeagues.Web/src/app/app.module.ts
+++ FitnessLeagues.Web/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DashboardHomeComponent } from "./Dashboard/dashboard.home";
 import {FitbitLoginComponent} from "./Login/fitbit.login"
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent, DashboardHomeComponent, FitbitLoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
